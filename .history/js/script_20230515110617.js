15fbcba887acac523cb9e83250ff336502afa324
20201576 <20201576@swe.com>
2023-05-15 11:21:36 +0900

    ENd of js

---
+++ .history/js/script_20230515110617.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $("navbar").addClass("sticky");
+    } else {
+      $("navbar").removeClass("sticky");
+    }
+  });
+});
+
+$(".menu-btn").click(function () {
+  $(".navbar .menu-bar").toggleClass("active");
+  $("menu-btn i").toggleClass("active");
+});
+
+var typed = new Typed(".typing1", {
+  strings: ["HTML", "Css", "Java Script", "React"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
